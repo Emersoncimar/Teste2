5f3c53de4e112b7e5fcb6223ae8a9c9f152bcfb0
Emersoncimar <ducho.ribeeiro@gmail.com>
2024-07-28 15:03:38 -0300

    tentaaai

--- core/settings.py
+++ core/settings.py
@@ -170,6 +170,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = ''
+EMAIL_HOST_USER = 'hahahehe'
+EMAIL_HOST_PASSWORD = 'emerosn'
+DEFAULT_FROM_EMAIL = 'rere'
+#Emerson mexendo novamente ente
